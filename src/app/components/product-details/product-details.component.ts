8c93d998fd951ce8607a32653be5ce297d99dde6
Muhammad Shahid <me911khan@gmail.com>
2025-04-02 09:15:02 +0200

    RouterLinks

--- src/app/components/product-details/product-details.component.ts
+++ src/app/components/product-details/product-details.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 
 @Component({
@@ -11,13 +11,12 @@ export class ProductDetailsComponent  {
 
   // private id!:number;
   id !: number;
-  name !: string;
+  
+  
+  // @Input() name!: string;
+  // @Input() description!: string;
 
   constructor(private route: ActivatedRoute) {
     this.id = this.route.snapshot.params['id'];
-  }
-
-  
-
-
+  } 
 }
